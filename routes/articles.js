e7d360085ff68b2b44be41965bc41af5af4bc1db
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-18 21:50:22 -0300

    adding hard-codded articles

---
+++ routes/articles.js
@@ -0,0 +1,11 @@
+const express = require( 'express');
+
+const router = express.Router();
+
+router.get('/test', (req, res, next) => {
+    res.send('In articles')
+});
+
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express() ;
+const articleRouter = require('./routes/articles')
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
 
 app.get('/',(req, res, next )=> {
-    res.send("hello world!")
+    const articles = [{
+        title: "Test Article",
+        createdAt: new Date(),
+        description: "Test description"
+    },
+    {
+        title: "Test Article2",
+        createdAt: new Date(),
+        description: "Test description2"
+    }
+]
+    res.render('index', { articles : articles })
 })
 app.listen(3000)
